acca2b9551cf24becae2a39a516c25a7deb73993
Thanuj V <tvasuthevan123@gmail.com>
2022-11-02 14:33:34 +0000

    Implemented CreatePrompt + tests

--- CreatePrompt/__init__.py
+++ CreatePrompt/__init__.py
@@ -1,28 +1,70 @@
 import logging
-import json 
+import json
 
 import azure.functions as func
 import azure.cosmos as cosmos
 import azure.cosmos.exceptions as exceptions
 import config
-
+import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Adding new user to db (RegisterPlayer)')
+    logging.info('Adding player to cloud DB')
 
     ## Local Setup
     client = cosmos.cosmos_client.CosmosClient(config.settings['db_URI'], config.settings['db_key'] )
 
     db_client = client.get_database_client(config.settings['db_id'])
 
+    prompts_container = db_client.get_container_client(config.settings['prompts_container'])
     players_container = db_client.get_container_client(config.settings['players_container'])
 
-    username = req.params.get('username')
-    pwd = req.params.get('pwd')
+    ## Cloud Setup
+    # client = cosmos.cosmos_client.CosmosClient(os.environ['db_URI'], os.environ['db_key'] )
+
+    # db_client = client.get_database_client(os.environ['db_id'])
+
+    # players_container = db_client.get_container_client(os.environ['players_container'])
+
+    prompt_info = req.get_json()
+
+    if len(prompt_info['text']) > 100 or len(prompt_info['text']) < 20:
+        return func.HttpResponse(body=json.dumps({"result": False, "msg": "prompt length is <20 or > 100 characters"}),
+            status_code = 400)
+
+    if len(prompt_info['password']) > 24 or len(prompt_info['password']) < 8 or len(prompt_info['username']) > 16 or len(prompt_info['username']) < 4:
+        return func.HttpResponse(body=json.dumps({"result": False, "msg": "bad username or password"}),
+            status_code = 400) 
 
 
 
-    return func.HttpResponse(
-        body=json.dumps({}),
-        status_code=200
-    )
+    try:
+        resp = list(players_container.query_items(
+            query='SELECT c.password FROM c WHERE c.id=@id',
+            parameters=[
+                {'name':'@id',  'value':prompt_info['username']}
+            ],
+            enable_cross_partition_query=True
+        ))
+
+        if len(resp)==1 and resp[0]['password'] == prompt_info['password']:
+            existing_prompt = list(prompts_container.query_items(
+                query='SELECT * FROM c WHERE c.username=@username AND c.text=@text',
+                parameters=[
+                    {'name':'@username',  'value':prompt_info['username']},
+                    {'name':'@text',  'value':prompt_info['text']}
+                ],
+                enable_cross_partition_query=True
+            ))
+            if len(existing_prompt) == 0:
+                del prompt_info['password']
+                prompts_container.create_item(prompt_info, enable_automatic_id_generation=True)
+                return func.HttpResponse(body=json.dumps({"result" : True, "msg": "OK" }),
+                status_code = 200)
+            else:
+                return func.HttpResponse(body=json.dumps({"result" : False, "msg": "This user already has a prompt with the same text" }),
+                status_code = 409)
+        else:
+            return func.HttpResponse(body=json.dumps({"result": False , "msg": "bad username or password"}),
+                status_code = 403)
+    except Exception as e:
+        repr(e)
---
+++ tests/test_create_prompt.py
@@ -0,0 +1,216 @@
+import unittest
+import json
+import requests 
+
+import azure.functions as func
+import azure.cosmos as cosmos
+import azure.cosmos.exceptions as exceptions
+import config
+#Important for the import name to match the case of the Function folder
+from CreatePrompt import main
+
+# Local setup
+client = cosmos.cosmos_client.CosmosClient(config.settings['db_URI'], config.settings['db_key'] )
+
+db_client = client.get_database_client(config.settings['db_id'])
+
+players_container = db_client.get_container_client(config.settings['players_container'])
+prompts_container = db_client.get_container_client(config.settings['prompts_container'])
+
+request_url = 'http://localhost:7071/api/prompt/create/'
+
+class TestFunction(unittest.TestCase):
+
+    ## Setup test players
+    ## Check items in prompts container
+    @classmethod
+    def setUpClass(cls) -> None:
+        try:
+            cls.maxDiff=None
+            players_container.create_item({'id': 'py_luis', 'password': 'pythonrulz'})
+            players_container.create_item({'id': 'py_thanuj', 'password': 'javascriptrulz'})
+            # if len(list(prompts_container.read_all_items()))>0:
+            #     raise Exception('Setup Failed - Empty propmts container before test')
+        except Exception as e:
+            print(repr(e))
+            raise unittest.SkipTest(repr(e))
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        try:
+            players_container.delete_item(item='py_luis', partition_key='py_luis')
+            players_container.delete_item(item='py_thanuj', partition_key='py_thanuj')
+        except Exception as e:
+            print(repr(e))
+
+    # Remove test prompt from container after each test
+    def tearDown(self):
+        try:
+            all_prompts = list(prompts_container.query_items(
+                query="SELECT c.id FROM c",
+                enable_cross_partition_query=True
+            ))
+            for prompt in all_prompts:
+                prompts_container.delete_item(item=prompt['id'], partition_key=prompt['id'])
+        except Exception as e:
+            print(repr(e))
+
+    def test_create_prompt__valid(self):
+        # Test adding same prompt by 2 different users to check it gets added to both.
+        payload = {"text": "What is an Azure function used for?", "username" : "py_luis", "password": "pythonrulz"}
+
+        resp = requests.post(
+            # Request URL (Use config)
+            request_url,
+            json=payload
+        )
+
+        create_response = resp.json()
+
+        all_prompts = list(prompts_container.read_all_items())
+        test_prompt = all_prompts[0]
+
+        del test_prompt['_attachments']
+        del test_prompt['_etag']
+        del test_prompt['_rid']
+        del test_prompt['_self']
+        del test_prompt['_ts']
+        del test_prompt['id']
+
+        expected_prompt = {
+            "username": "py_luis",
+            "text": "What is an Azure function used for?"
+        }
+
+        expected_req_resp = {"result" : True, "msg": "OK" }
+
+        self.assertEqual(expected_prompt, test_prompt)
+        self.assertEqual(expected_req_resp, create_response)
+
+        # Same text, different user
+        payload = {"text": "What is an Azure function used for?", "username" : "py_thanuj", "password": "javascriptrulz"}
+
+        resp = requests.post(
+            # Request URL (Use config)
+            request_url,
+            json=payload
+        )
+
+        create_response = resp.json()
+
+        all_prompts = list(prompts_container.read_all_items())
+        test_prompt = all_prompts[1]
+
+        del test_prompt['_attachments']
+        del test_prompt['_etag']
+        del test_prompt['_rid']
+        del test_prompt['_self']
+        del test_prompt['_ts']
+        del test_prompt['id']
+
+        expected_prompt = {
+            "username": "py_thanuj",
+            "text": "What is an Azure function used for?"
+        }
+
+        expected_req_resp = {"result" : True, "msg": "OK" }
+
+        self.assertEqual(expected_prompt, test_prompt)
+        self.assertEqual(expected_req_resp, create_response)
+
+    def test_create_prompt__duplicate(self):
+        existing_prompt = {
+            "id": "0",
+            "text": "What is an Azure function used for?",
+            "username": "py_luis"
+        }
+
+        prompts_container.create_item(existing_prompt)
+
+        payload={
+            "text": "What is an Azure function used for?",
+            "username": "py_luis",
+            "password": "pythonrulz"
+        }
+
+        resp = requests.post(
+            # Request URL ((Use config)
+            request_url,
+            json=payload
+        )
+
+        create_response = resp.json()
+
+        expected_req_resp = {"result" : False, "msg": "This user already has a prompt with the same text" }
+
+        self.assertEqual(expected_req_resp, create_response)
+
+    def test_create_prompt__invalid_prompt(self):
+        # Long prompt
+        payload = {"text": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean ma", "username" : "py_luis", "password": "pythonrulz"}
+
+        resp = requests.post(
+            # Request URL ((Use config)
+            request_url,
+            json=payload
+        )
+
+        create_response = resp.json()
+
+        expected_req_resp = {"result" : False, "msg": "prompt length is <20 or > 100 characters" }
+
+        self.assertEqual(expected_req_resp, create_response)
+
+        ## Short prompt
+        payload = {"text": "Short prompt", "username" : "py_luis", "password": "pythonrulz"}
+
+        resp = requests.post(
+            # Request URL ((Use config)
+            request_url,
+            json=payload
+        )
+
+        resp = requests.post(
+            # Request URL ((Use config)
+            request_url,
+            json=payload
+        )
+
+        create_response = resp.json()
+
+        expected_req_resp = {"result" : False, "msg": "prompt length is <20 or > 100 characters" }
+
+        self.assertEqual(expected_req_resp, create_response)
+
+    def test_create_prompt__invalid_user(self):
+        # Incorrect password
+        payload = {"text": "Good question to be inserted here?", "username" : "py_luis", "password": "pythonwrong"}
+
+        resp = requests.post(
+            # Request URL ((Use config)
+            request_url,
+            json=payload
+        )
+
+        create_response = resp.json()
+
+        expected_req_resp = {"result" : False, "msg": "bad username or password" }
+
+        self.assertEqual(expected_req_resp, create_response)
+
+        # Nonexistent username
+        payload = {"text": "Good question to be inserted here?", "username" : "py_nonexistent", "password": "pythonwrong"}
+
+        resp = requests.post(
+            # Request URL ((Use config)
+            request_url,
+            json=payload
+        )
+
+        create_response = resp.json()
+
+        expected_req_resp = {"result" : False, "msg": "bad username or password" }
+
+        self.assertEqual(expected_req_resp, create_response)
+
+        
