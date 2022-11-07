e59f0746774b32e6bcd0ca45de6d227ef95e1115
Thanuj V <tvasuthevan123@gmail.com>
2022-11-07 15:28:27 +0000

    config change (recreated resources)

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 settings = {
+    'APP_KEY': '3VdhNGKZ2BCczvxrUHp_GhZq3o40XAl9Mt9cJp5jwRDqAzFueFk7vQ==',
     'local_URI': 'http://localhost:7071/api/' ,
-    'cloud_URI' : 'https://treehuggers-vt2g19.azurewebsites.net' ,
+    'cloud_URI' : 'https://quippylash-vt2g19.azurewebsites.net/api/',
     'db_URI' : 'https://vt2g19.documents.azure.com:443/',
-    'db_key' : 'IcbzRZaf2oDcIB9jyHyx1NJkQjYvTcutuLKa7bbsu3XOVVbIbaNCULeHC9hDndu1U0qHD5Wk0CRTQKvEKwT7Yw==',
+    'db_key' : 'n6GISX9JTndR1dbUMMgNsZcSOd6sGIVUG3ilIV0DGajd73sNCUJW52CR2hBhhOMpaBCos9Hw9VcCACDbyFBv8A==',
     'db_id' : 'quippylash' , # I used treeHuggers2223, you may have used other
     'players_container': 'players',
     'prompts_container' : 'prompts'
