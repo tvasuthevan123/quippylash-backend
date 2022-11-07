4c62dabba27d856bf3ab1b7ade8354ee9395ac1b
Thanuj V <tvasuthevan123@gmail.com>
2022-11-07 12:30:36 +0000

    Implemented GetTextPrompts + tests

--- GetTextPrompts/__init__.py
+++ GetTextPrompts/__init__.py
@@ -1,24 +1,52 @@
 import logging
+import json
+import random
 
 import azure.functions as func
-
+import azure.cosmos as cosmos
+import azure.cosmos.exceptions as exceptions
+import config
+import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Get prompts by text')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
+    ## Local Setup
+    client = cosmos.cosmos_client.CosmosClient(config.settings['db_URI'], config.settings['db_key'] )
+
+    db_client = client.get_database_client(config.settings['db_id'])
+
+    prompts_container = db_client.get_container_client(config.settings['prompts_container'])
+
+    ## Cloud Setup
+    # client = cosmos.cosmos_client.CosmosClient(os.environ['db_URI'], os.environ['db_key'] )
+
+    # db_client = client.get_database_client(os.environ['db_id'])
+
+    # players_container = db_client.get_container_client(os.environ['players_container'])
+
+    requested = req.get_json()
+            
+    try:
+        if(requested['word']!=""):
+            user_prompts = list(prompts_container.query_items(
+                query='SELECT c.id, c.text, c.username FROM c WHERE RegexMatch(c.text, @regex, @case)=true',
+                parameters=[
+                    {'name':'@regex',  'value':"(?<![^\\W_])"+ requested['word'] +"(?![^\\W_])"},
+                    {'name':'@case', 'value': ('i', '')[requested['exact']]}
+                ],
+                enable_cross_partition_query=True
+            ))
+            return func.HttpResponse(body=json.dumps(user_prompts), status_code = 200)
         else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+            all_prompts = list(map(
+            lambda x: {
+                'id': x['id'], 
+                'username': x['username'], 
+                'text': x['text']
+            },
+            prompts_container.read_all_items()))
+            return func.HttpResponse(body=json.dumps(all_prompts), status_code=200)
+    except exceptions.CosmosHttpResponseError:
+        return func.HttpResponse(body=json.dumps([]),
+            status_code = 500)
---
+++ tests/test_get_text_prompts.py
@@ -0,0 +1,131 @@
+import unittest
+import json
+import requests 
+
+import azure.functions as func
+import azure.cosmos as cosmos
+import azure.cosmos.exceptions as exceptions
+import config
+#Important for the import name to match the case of the Function folder
+from LoginPlayer import main
+
+# Local setup
+client = cosmos.cosmos_client.CosmosClient(config.settings['db_URI'], config.settings['db_key'] )
+
+db_client = client.get_database_client(config.settings['db_id'])
+
+prompts_container = db_client.get_container_client(config.settings['prompts_container'])
+
+request_url = 'http://localhost:7071/api/prompts/getText/'
+
+class TestFunction(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls) -> None:
+        try:
+            prompts_container.create_item({"id": "0" , "text" : "What Program you would never code in JavaScript", "username": "py_luis"})
+            prompts_container.create_item({"id": "1" , "text" : "What is the funniest programming language?", "username": "js_oli"})
+            prompts_container.create_item({"id": "2" , "text" : "How many lines your shorter program has?", "username": "perl_les"})
+        except Exception as e:
+            # print(repr(e))
+            pass
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        try:
+            prompts_container.delete_item(item="0", partition_key="0")
+            prompts_container.delete_item(item="1", partition_key="1")
+            prompts_container.delete_item(item="2", partition_key="2")
+        except Exception as e:
+            # print(repr(e))
+            pass
+
+    def test_get_text_prompts__case_insensitive(self):
+        payload ={
+            "word": "program",
+            "exact": True
+        }
+
+        resp = requests.post(
+            # Request URL (Use config)
+            request_url,
+            json=payload
+        )
+
+        get_response = list(resp.json())
+        expected_response = [{"id": "2" , "text" : "How many lines your shorter program has?", "username": "perl_les"}]
+
+        self.assertEqual(expected_response, get_response)
+
+        payload ={
+            "word": "Program",
+            "exact": True
+        }
+
+        resp = requests.post(
+            # Request URL (Use config)
+            request_url,
+            json=payload
+        )
+
+        get_response = resp.json()
+        expected_response = [{"id": "0" , "text" : "What Program you would never code in JavaScript", "username": "py_luis"}]
+
+        self.assertEqual(expected_response, get_response)
+
+        payload ={
+            "word": "language",
+            "exact": True
+        }
+
+        resp = requests.post(
+            # Request URL (Use config)
+            request_url,
+            json=payload
+        )
+
+        get_response = list(resp.json())
+        expected_response = [{"id": "1" , "text" : "What is the funniest programming language?", "username": "js_oli"}]
+
+        self.assertEqual(expected_response, get_response)
+
+
+
+    def test_get_text_prompts__case_sensitive(self):
+        payload ={
+            "word": "program",
+            "exact": False
+        }
+
+        resp = requests.post(
+            # Request URL (Use config)
+            request_url,
+            json=payload
+        )
+
+        get_response = list(resp.json())
+        expected_response = [
+            {"id": "0" , "text" : "What Program you would never code in JavaScript", "username": "py_luis"},
+            {"id": "2" , "text" : "How many lines your shorter program has?", "username": "perl_les"}
+        ]
+
+        self.assertEqual(expected_response, get_response)
+
+        payload ={
+            "word": "progrAmMIng",
+            "exact": False
+        }
+
+        resp = requests.post(
+            # Request URL (Use config)
+            request_url,
+            json=payload
+        )
+
+        get_response = list(resp.json())
+        expected_response = [
+            {"id": "1" , "text" : "What is the funniest programming language?", "username": "js_oli"}
+        ]
+
+        self.assertEqual(expected_response, get_response)
+
